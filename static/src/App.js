cdcef3c5dd6d1f4848c5c15ee7965da0acd6e77b
mac <seanmcilroy89@gmail.com>
2018-11-24 07:04:09 -0600

    Routing for calcs in place

--- static/src/App.js
+++ static/src/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
-import { MaxesForm } from './containers/MaxesForm'
+import { MaxesForm } from './containers/MaxesForm';
+import { BodyCalculators } from './containers/BodyCalculators';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-          <div className="row">
-            <div className="col-4"></div>
-              <MaxesForm />
-            <div className="col-4"></div>
-          </div>
+      <div>
+          <Router>
+            <div>
+              <NavBar />
+              <div className="row">
+                <div className="col-5"></div>
+                    <Switch>
+                      <Route exact path='/' component={MaxesForm} />
+                      <Route path='/calculators' component={BodyCalculators} />
+                    </Switch>
+                <div className="col-3"></div>
+              </div>
+            </div>
+          </Router>
       </div>
     );
+  }
+}
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+      <nav className="navbar navbar-expand-lg navbar-dark  bg-dark">
+        <Link className="navbar-brand" to="/">TheMcIlroy</Link>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Routine</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/calculators">Calculators</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    )
   }
 }
 
---
+++ static/src/containers/BodyCalculators.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class BodyCalculators extends Component {
+    render() {
+        return (
+            <p>Let's calculate!</p>
+        )
+    }
+}
